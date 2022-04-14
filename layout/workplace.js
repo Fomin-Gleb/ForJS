103c060365d73fe33173a3c455af20d5a7ef3dc7
Fomin-Gleb <fominglebdev@gmail.com>
2022-04-14 20:21:13 +0300

    all

---
+++ layout/workplace.js
@@ -0,0 +1,37 @@
+// alert('Workplace - HERE')
+
+// fibarr = [0, 1, 1, 2];
+
+// let x = 0;
+// // let y = 1;
+
+// while (fibarr.lenght < 100) {
+//     // fibarr.push(x+y);
+//     x = fibarr[fibarr.length - 1] + fibarr[fibarr.length - 2]
+//     fibarr.push(x);
+// }
+// // consolse.log(fibarr);
+// for(i=0 ; i<=fibarr.lenght-1 ;i++){
+//     console.log(fibarr[i]);
+// }
+// 0 1 
+// let x = 0n; 
+// let y = 1n; 
+// let z = 0n; 
+// const fibbArray = [1n] 
+// for(let i = 0;i<33;i++){ 
+ 
+//     z = BigInt(x + y); 
+//     fibbArray.push(z) 
+ 
+//     x = BigInt(y + z); 
+//     fibbArray.push(x) 
+ 
+//     y = BigInt(z + x); 
+//     fibbArray.push(y) 
+//     console.log(fibbArray) 
+// } 
+ 
+// console.log(fibbArray[fibbArray.length - 1]) 
+// console.log(fibbArray.length)
+
